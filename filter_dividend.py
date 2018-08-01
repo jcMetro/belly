3433778876684c800b5bbbb8a90e5142ffb80ea5
johnnychow <johnnychow@octfinancial.com>
2018-08-01 17:47:59 +0800

    add stock into a list if eligible

--- filter_dividend.py
+++ filter_dividend.py
@@ -5,17 +5,19 @@ db = MongoClient().belly
 
 stocks = db.dividends.distinct("stock")
 
+increase_div_stocks = []
+
 for stock in stocks:
     periods = db.dividends.find({"stock": stock}).distinct("period")
     if len(periods) < 5:
-        print(f'stock {stock} does not have 5 years div payout')
+        # print(f'stock {stock} does not have 5 years div payout')
         continue
 
     recent_periods = sorted(periods, reverse=True)[:5]
     non_continuous_pay_out = any((int(recent_periods[i]) - int(recent_periods[i + 1]) != 1) for i in range(4))
 
     if non_continuous_pay_out:
-        print(f'stock {stock} stop pay dividend for some years')
+        # print(f'stock {stock} stop pay dividend for some years')
         continue
 
     out = db.dividends.aggregate([
@@ -26,12 +28,8 @@ for stock in stocks:
     ])
 
     decimal_div = [item['total'].to_decimal() for item in list(out)]
-    pprint(decimal_div)
     passed = all((decimal_div[i] - decimal_div[i+1] >= 0)for i in range(4))
-    print(f'stock {stock} result: {passed}')
+    if passed:
+        increase_div_stocks.append(stock)
 
-# pprint.pprint(list(db.dividends.aggregate(pipeline)))
-
-# for each stock
-#    retrieve most recent 5 years dividend
-#    if dividend increased on 4 out of 5 years, passed
+pprint(increase_div_stocks)
